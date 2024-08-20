4506d38df2a452fb0579e54c5120a6a9c2bf482e
choeyejun <inVeryiIrLFVhNy9U7ltXktfxS4IFstgY4avstXTUE8=>
2024-08-20 14:50:46 +0900

    fix : syntax error update

--- webhook/src/main/java/com/yejun/webhook/WebhookApplication.java
+++ webhook/src/main/java/com/yejun/webhook/WebhookApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebhookApplication {
 
 	public static void main(String[] args) {
-//		int[] temp = new int[]{14};
-//		System.out.println(temp[15]);
 		SpringApplication.run(WebhookApplication.class, args);
 	}
 
