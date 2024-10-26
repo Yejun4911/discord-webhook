d298c91e19edeb9267e2dc2181ad6f8932a1e6c5
yejun4911 <yejun4911@naver.com>
2024-10-26 19:39:42 +0900

    feat : discord-webhook

--- webhook/src/main/java/com/yejun/webhook/controller/ErrorController.java
+++ webhook/src/main/java/com/yejun/webhook/controller/ErrorController.java
@@ -1,4 +1,4 @@
-package com.yejun.webhook;
+package com.yejun.webhook.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
